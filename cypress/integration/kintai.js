c59f7846d363db8060f786f942edca81343088bd
tetsuyaohira <tetsuya.ohira@icloud.com>
2022-04-02 08:56:50 +0900

    first commit

---
+++ cypress/integration/kintai.js
@@ -0,0 +1,19 @@
+describe('kintai', () => {
+    beforeEach(() => {
+        cy.visit('https://s2.kingtime.jp/independent/recorder/personal/')
+    })
+    it('displays two todo items by default', () => {
+        cy.get('#id').type(Cypress.env('login_id'))
+        cy.get('#password').type(Cypress.env('login_password'))
+        cy.get('.btn-control-message').click()
+
+        const dt = new Date();
+        if (dt.getHours() === 9) {
+            // 出勤
+            // cy.contains('出勤').click()
+        } else if (dt.getHours() === 18) {
+            // 退勤
+            // cy.contains('退勤').click()
+        }
+    })
+})
